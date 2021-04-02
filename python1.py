ee961a69339d2c60a8be22d702906f8f1e24b5b4
ashishdhanve <ashishdhanve116@gmail.com>
2021-04-02 13:50:10 +0530

    retrived cover.jpg from web
    accessing http://data.pr4e.org/

--- python1.py
+++ python1.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+img = urllib.request.urlopen("http://data.pr4e.org/cover.jpg").read()
+fhand = open("cover.jpg","wb")
+fhand.write(img)
+fhand.close()
+
+fhand1 = urllib.request.urlopen('http://data.pr4e.org/')
+soup = BeautifulSoup(fhand1, 'html.parser')
+
+for l in soup:
+    print(l)
